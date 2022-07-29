0902315a4eded3392d2e068f9c793a338b5953ce
Anan166 <73787000+Anan166@users.noreply.github.com>
2022-07-29 20:46:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var bg;
+var pingu, pinguImage;
+var snow =[];
+
+function preload() {
+  bg = loadImage("snow0.png");
+  pinguImage = loadImage("pingu.png");
+}
+
+function setup() {
+
+  createCanvas(1200,800);
+  
+
+  for (var i = 0; i < 15; i++) {
+    snow.push(new Snow());
+  }
+  var widthheight = (random(0, 512))
+  snow = new Snow(random(0, 960), random(0, 678), widthheight, widthheight, random(0, 360));
+  pingu=createSprite(200,600)
+  pingu.addImage(pinguImage)
+  pingu.scale=0.2
+}
+
+function draw() {
+
+
+  background(bg);
+  
+  for (var i = 0; i < snow.length; i++) {
+    snow[i].display();
+    snow[i].update();
+  }
+
+  snow.display();
+  drawSprites()
+}
